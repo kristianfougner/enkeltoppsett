65d4c885a7c74e2c7dabcce735bd4f60ae987037
kristianfougner <kristianfougner@gmail.com>
2021-03-17 18:03:15 +0000

    strukturert prosjektet med maven, gjort endringer i .gitignore og pom.xml

---
+++ enkeltoppsett/src/main/java/enkeltoppsett/App.java
@@ -0,0 +1,13 @@
+package enkeltoppsett;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
